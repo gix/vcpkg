56ec9296902d6b026f5617cbf7a99b896c3e3fbb
Nico Rieck <nico.rieck@gmail.com>
2020-11-29 18:53:36 +0100

    [outcome] Fix MSVC compiler error in C++20 mode

--- ports/outcome/portfile.cmake
+++ ports/outcome/portfile.cmake
@@ -5,6 +5,8 @@ vcpkg_from_github(
     REF e64a5539c7b0416184cf9c7f82aeacc988b01229
     SHA512 05a759503891e7d12e1ef8b154a4e86fe282c7a69de3137c73138da2294f5e0dea7d90eda8071666b46e973e522a827a27d036bf5d0c7c1c2e1d5e871e4a1db9
     HEAD_REF develop
+    PATCHES
+        fix-msvc-compiler-error.patch
 )
 
 file(GLOB_RECURSE OUTCOME_HEADERS "${SOURCE_PATH}/single-header/*.hpp")
